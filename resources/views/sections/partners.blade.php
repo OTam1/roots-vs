82fb2271820f2929886892fb52b41511c35b5d49
IT@temtem-Tech.com <115486578+Temtem-Tech@users.noreply.github.com>
2024-09-14 20:03:31 +0300

    Add partners

--- resources/views/sections/partners.blade.php
+++ resources/views/sections/partners.blade.php
@@ -1,4 +1,4 @@
-<div class="carousel-wrapper">
+{{-- <div class="carousel-wrapper">
     <h1 class="partners_text">Our Strategic Partners</h1>
     <div class="owl-carousel partners owl-theme owl-custom">
         <div class="item carousel-item"><img src="{{ '/assets/img/Revival_Lab.png' }}"
@@ -19,4 +19,83 @@
         </div>
 
     </div>
-</div>
+</div> --}}
+
+<div class="carousel-wrapper">
+        <h1 class="partners_text">Our Ecosystem</h1>
+        <div class="owl-carousel partners owl-theme owl-custom">
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/2290501-482200157.jpg' }}"
+                    alt="Partner 1"></div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/AQWA_POWER.png' }}" alt="Partner 2">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/Aramco_Taleed.webp' }}"
+                    alt="Partner 3"></div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/Artboard_6.webp' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/cdfb8e_0afb9c9df0234fae828b83302aafc69d~mv2.png' }}"
+                    alt="Partner 1"></div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/Rubber.jpeg' }}" alt="Partner 2">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/GITEX.png' }}"
+                    alt="Partner 3"></div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/GREEN_TECH.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/headerar.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/King-Saud-University-01.svg' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/logo_retina-1-1.svg' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/logo-s.svg' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/Misk-01-01.svg' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/mit.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/Monshaat-01.svg' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/netothon.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/New-Momrah-logo-1-s.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/nre-footer-logo.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/rfi_logo.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/RLHN5p0d_400x400.jpg' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/saudi_arabia.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/SAUDI_ARAMCO.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/SIDF.svg' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/tadweeer.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/TC1MAMtP_400x400.jpg' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/UN_GLOBAL.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/uplink.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/Arabi_tadweer.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/KAU.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/Susti.webp' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/FLK.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/MODON.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/FindSoc.jpg' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/Wadi_dahran.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/Waaed.png' }}" alt="Partner 4">
+            </div>
+            <div class="item carousel-item"><img src="{{ '/assets/img/ecosystem/Minsteryofpower.png' }}" alt="Partner 4">
+            </div>
+
+        </div>
+    </div>
