bffc28125b1278d8d9c91bed48f7bc17c1ef6d18
IT@temtem-Tech.com <115486578+Temtem-Tech@users.noreply.github.com>
2024-10-23 17:05:48 +0300

    about us fix

--- resources/views/sections/about/aboutus.blade.php
+++ resources/views/sections/about/aboutus.blade.php
@@ -1,7 +1,7 @@
 <section class="lqd-section text-box-single pt-120" data-section-luminosity="light">
     <div class="container px-15">
         <div class="row" style="place-items: center;place-content: center;">
-            <div class="col col-6 col-md-6" style="margin-inline-end: auto;">
+            <div class="col col-6 col-md-6">
                 <div class="ld-fancy-heading">
                     <h1 class="ld-fh-element mb-0/75em text-48 -tracking-0/5">@lang('gettoknowus.about')</h1>
                 </div>
@@ -11,9 +11,9 @@
                     </p>
                 </div>
             </div>
-            <div class="col col-4 col-md-4">
+            <div class="col col-6 col-md-6">
                 <div class="ld-fancy-heading">
-                    <img class="w-90percent rounded-20" src="assets/img/Roots.png">
+                    <img class="w-90percent rounded-20" src="assets/img/roots_aboutus.png">
                 </div>
             </div>
         </div>
@@ -21,9 +21,9 @@
 </section>
 <section class="lqd-section icon-box-text py-40 ca-initvalues-applied lqd-animations-done" data-section-luminosity="light" data-custom-animations="true" data-ca-options="{&quot;triggerHandler&quot;: &quot;inview&quot;, &quot;animationTarget&quot;: &quot;.animation-element&quot;, &quot;duration&quot;: &quot;1000&quot;, &quot;delay&quot;: &quot;225&quot;, &quot;ease&quot;: &quot;power4.out&quot;, &quot;direction&quot;: &quot;forward&quot;, &quot;initValues&quot;: {&quot;transformOriginX&quot; : 50 , &quot;transformOriginY&quot; : 50 , &quot;transformOriginZ&quot;: &quot;0px&quot;, &quot;opacity&quot; : 0} , &quot;animations&quot;: {&quot;transformOriginX&quot; : 50 , &quot;transformOriginY&quot; : 50 , &quot;transformOriginZ&quot;: &quot;0px&quot;, &quot;opacity&quot; : 1}}">
     <div class="container">
-        <div class="row" style="place-content: center;">
-            <div class="col col-md-5 col-lg-5 col-12">
-                <div class="iconbox mb-30 flex flex-col relative justify-center items-start text-start animation-element lqd-unit-animation-done" style="">
+        <div class="row">
+            <div class="col col-md-3 col-lg-3 col-12">
+                <div class="iconbox mb-30 flex flex-col relative items-start text-start animation-element lqd-unit-animation-done" style="">
                     <div class="iconbox-icon-wrap"><span class="iconbox-icon-container mb-30 text-50 leading-1em text-primary transition-all">
                         {{-- <i class="lqd-icn-ess icon-lqd-eye"></i> --}}
                         <img class="lqd-icn-ess icon-lqd-eye aboutusicons" src="assets/img/aboutus-icons/vision.png" width="20"/>
@@ -34,8 +34,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col col-md-5 col-lg-5 col-12">
-                <div class="iconbox mb-30 flex flex-col relative justify-center items-start text-start animation-element lqd-unit-animation-done" style="">
+            <div class="col col-md-3 col-lg-3 col-12">
+                <div class="iconbox mb-30 flex flex-col relative items-start text-start animation-element lqd-unit-animation-done" style="">
                     <div class="iconbox-icon-wrap"><span class="iconbox-icon-container mb-30 text-50 leading-1em text-primary transition-all">
                         {{-- <i class="lqd-icn-ess icon-lqd-path"></i> --}}
                         <img class="lqd-icn-ess icon-lqd-eye aboutusicons" src="assets/img/aboutus-icons/mission.png" width="20"/>
