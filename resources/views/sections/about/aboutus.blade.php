ca90e9367c58f797df79ee08f1ea8fb3be7c57f5
IT@temtem-Tech.com <115486578+Temtem-Tech@users.noreply.github.com>
2024-10-28 08:52:36 +0300

    about us re design

--- resources/views/sections/about/aboutus.blade.php
+++ resources/views/sections/about/aboutus.blade.php
@@ -1,7 +1,8 @@
 <section class="lqd-section text-box-single pt-120" data-section-luminosity="light">
     <div class="container px-15">
-        <div class="row" style="place-items: center;place-content: center;">
-            <div class="col col-6 col-md-6">
+        <div class="row" style="display: flex; align-items: start; place-content: center;">
+            <div class="col col-6 col-md-6" style="display: flex; flex-direction: column;">
+                <!-- About Section -->
                 <div class="ld-fancy-heading">
                     <h1 class="ld-fh-element mb-0/75em text-48 -tracking-0/5">@lang('gettoknowus.about')</h1>
                 </div>
@@ -10,41 +11,44 @@
                         @lang('gettoknowus.about_desc')
                     </p>
                 </div>
-            </div>
-            <div class="col col-6 col-md-6">
-                <div class="ld-fancy-heading">
-                    <img class="w-90percent rounded-20" src="assets/img/roots_aboutus.png">
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<section class="lqd-section icon-box-text py-40 ca-initvalues-applied lqd-animations-done" data-section-luminosity="light" data-custom-animations="true" data-ca-options="{&quot;triggerHandler&quot;: &quot;inview&quot;, &quot;animationTarget&quot;: &quot;.animation-element&quot;, &quot;duration&quot;: &quot;1000&quot;, &quot;delay&quot;: &quot;225&quot;, &quot;ease&quot;: &quot;power4.out&quot;, &quot;direction&quot;: &quot;forward&quot;, &quot;initValues&quot;: {&quot;transformOriginX&quot; : 50 , &quot;transformOriginY&quot; : 50 , &quot;transformOriginZ&quot;: &quot;0px&quot;, &quot;opacity&quot; : 0} , &quot;animations&quot;: {&quot;transformOriginX&quot; : 50 , &quot;transformOriginY&quot; : 50 , &quot;transformOriginZ&quot;: &quot;0px&quot;, &quot;opacity&quot; : 1}}">
-    <div class="container">
-        <div class="row">
-            <div class="col col-md-3 col-lg-3 col-12">
-                <div class="iconbox mb-30 flex flex-col relative items-start text-start animation-element lqd-unit-animation-done" style="">
-                    <div class="iconbox-icon-wrap"><span class="iconbox-icon-container mb-30 text-50 leading-1em text-primary transition-all">
-                        {{-- <i class="lqd-icn-ess icon-lqd-eye"></i> --}}
-                        <img class="lqd-icn-ess icon-lqd-eye aboutusicons" src="assets/img/aboutus-icons/vision.png" width="20"/>
-                    </span></div>
-                    <div class="contents">
-                        <h3 class="font-semibold uppercase mb-10 text-20 text-black"> @lang('gettoknowus.vision')</h3>
-                        <p class="text-18 text-gray-400 font-medium"><span>@lang('gettoknowus.vision_desc')</span></p>
+
+                <!-- Vision and Mission Sections Side-by-Side -->
+                <div class="row" style="margin-top: 2rem;">
+                    <!-- Vision Section -->
+                    <div class="col col-6 col-md-6">
+                        <div class="iconbox mb-30 flex flex-col relative items-start text-start animation-element lqd-unit-animation-done">
+                            <div class="iconbox-icon-wrap">
+                                <span class="iconbox-icon-container mb-20 text-50 leading-1em text-primary transition-all">
+                                    <img class="lqd-icn-ess icon-lqd-eye aboutusicons" src="assets/img/aboutus-icons/vision.png" width="30" />
+                                </span>
+                            </div>
+                            <div class="contents">
+                                <h3 class="font-semibold uppercase mb-10 text-20 text-black">@lang('gettoknowus.vision')</h3>
+                                <p class="text-18 text-gray-400 font-medium">@lang('gettoknowus.vision_desc')</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Mission Section -->
+                    <div class="col col-6 col-md-6">
+                        <div class="iconbox mb-30 flex flex-col relative items-start text-start animation-element lqd-unit-animation-done">
+                            <div class="iconbox-icon-wrap">
+                                <span class="iconbox-icon-container mb-20 text-50 leading-1em text-primary transition-all">
+                                    <img class="lqd-icn-ess icon-lqd-eye aboutusicons" src="assets/img/aboutus-icons/mission.png" width="30" />
+                                </span>
+                            </div>
+                            <div class="contents">
+                                <h3 class="font-semibold uppercase mb-10 text-20 text-black">@lang('gettoknowus.mission')</h3>
+                                <p class="text-18 text-gray-400 font-medium">@lang('gettoknowus.mission_desc')</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col col-md-3 col-lg-3 col-12">
-                <div class="iconbox mb-30 flex flex-col relative items-start text-start animation-element lqd-unit-animation-done" style="">
-                    <div class="iconbox-icon-wrap"><span class="iconbox-icon-container mb-30 text-50 leading-1em text-primary transition-all">
-                        {{-- <i class="lqd-icn-ess icon-lqd-path"></i> --}}
-                        <img class="lqd-icn-ess icon-lqd-eye aboutusicons" src="assets/img/aboutus-icons/mission.png" width="20"/>
-                    </span></div>
-                    <div class="contents">
-                        <h3 class="font-semibold uppercase mb-10 text-20 text-black">@lang('gettoknowus.mission')</h3>
-                        <p class="text-18 text-gray-400 font-medium"><span>@lang('gettoknowus.mission_desc')</span></p>
-                    </div>
-                </div>
+
+            <!-- Image Section (right side) -->
+            <div class="col col-6 col-md-6" style="display: flex; align-items: center; justify-content: center;">
+                <img class="w-70percent rounded-20" src="assets/img/Roots.png" style="max-width: 100%; height: auto; border-radius: 20px;">
             </div>
         </div>
     </div>
