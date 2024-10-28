db1f5309198c438d91e0ea43d1b6bdabe03782b9
IT@temtem-Tech.com <115486578+Temtem-Tech@users.noreply.github.com>
2024-10-28 11:11:21 +0300

    about us fix

--- lang/en/gettoknowus.php
+++ lang/en/gettoknowus.php
@@ -3,11 +3,11 @@
 return [
     //about
     'about' => 'About ROOTS',
-    'about_desc' => 'ROOTS is the first venture studio in Saudi Arabia that focuses on climate tech and sustainability.<br> ROOTS aims to open investment opportunities, localize advanced technologies, provide job opportunities for promising Saudi talents, and attract global investments. This contributes to achieving environmental sustainability goals in the Kingdom and worldwide.',
+    'about_desc' => 'ROOTS is the first venture studio in Saudi Arabia that focuses on climate tech and sustainability. ROOTS aims to open investment opportunities, localize advanced technologies, provide job opportunities for promising Saudi talents, and attract global investments. This contributes to achieving environmental sustainability goals in the Kingdom and worldwide.',
     'vision' => 'Vision',
     'vision_desc' => 'To become the key enabler for sustainability innovation in the country and in the region.',
     'mission' => 'Mission',
-    'mission_desc' => 'To Build impact driven ventures that address climate change challenges, unlock untapped opportunities, and contribute to achieving sustainability, local and global, ambitions.',
+    'mission_desc' => 'Building ventures that address climate challenges, open doors to opportunities, and contribute to achieving local and global sustainability aspirations.',
     
     
     //values
