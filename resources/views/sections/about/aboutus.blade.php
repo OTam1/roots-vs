ff4cec8646f839dfe2615c67c34a500ecb541c2a
IT@temtem-Tech.com <115486578+Temtem-Tech@users.noreply.github.com>
2024-10-28 12:31:06 +0300

    about us fix

--- resources/views/sections/about/aboutus.blade.php
+++ resources/views/sections/about/aboutus.blade.php
@@ -4,10 +4,10 @@
             <div class="col col-6 col-md-6" style="display: flex; flex-direction: column;">
                 <!-- About Section -->
                 <div class="ld-fancy-heading">
-                    <h1 class="ld-fh-element mb-0/75em text-48 -tracking-0/5 text-primary">@lang('gettoknowus.about')</h1>
+                    <h1 class="ld-fh-element mb-0/75em text-38 -tracking-0/5 text-primary">@lang('gettoknowus.about')</h1>
                 </div>
                 <div class="ld-fancy-heading">
-                    <p class="ld-fh-element mb-0/5em leading-1/5em text-20 md:pr-0">
+                    <p class="ld-fh-element mb-0/5em leading-1/5em text-17 md:pr-0">
                         @lang('gettoknowus.about_desc')
                     </p>
                 </div>
@@ -15,8 +15,8 @@
                 <!-- Vision and Mission Sections Side-by-Side -->
                 <div class="row" style="margin-top: 2rem;">
                     <!-- Vision Section -->
-                    <div class="col col-6 col-md-6">
-                        <div class="iconbox mb-30 flex flex-col relative items-start text-start animation-element lqd-unit-animation-done">
+                    <div class="col col-5 col-md-5">
+                        <div class="iconbox mt-30 flex flex-col relative items-start text-start animation-element lqd-unit-animation-done">
                             <div class="iconbox-icon-wrap">
                                 <span class="iconbox-icon-container mb-20 text-50 leading-1em text-primary transition-all">
                                     <img class="lqd-icn-ess icon-lqd-eye aboutusicons" src="assets/img/aboutus-icons/vision.png" width="30" />
@@ -24,14 +24,14 @@
                             </div>
                             <div class="contents">
                                 <h3 class="font-semibold uppercase mb-10 text-20 text-black">@lang('gettoknowus.vision')</h3>
-                                <p class="text-16 text-gray-400 font-medium">@lang('gettoknowus.vision_desc')</p>
+                                <p class="text-15 text-gray-400 font-medium">@lang('gettoknowus.vision_desc')</p>
                             </div>
                         </div>
                     </div>
 
                     <!-- Mission Section -->
                     <div class="col col-6 col-md-6">
-                        <div class="iconbox mb-30 flex flex-col relative items-start text-start animation-element lqd-unit-animation-done">
+                        <div class="iconbox mt-30 flex flex-col relative items-start text-start animation-element lqd-unit-animation-done">
                             <div class="iconbox-icon-wrap">
                                 <span class="iconbox-icon-container mb-20 text-50 leading-1em text-primary transition-all">
                                     <img class="lqd-icn-ess icon-lqd-eye aboutusicons" src="assets/img/aboutus-icons/mission.png" width="30" />
@@ -39,7 +39,7 @@
                             </div>
                             <div class="contents">
                                 <h3 class="font-semibold uppercase mb-10 text-20 text-black">@lang('gettoknowus.mission')</h3>
-                                <p class="text-16 text-gray-400 font-medium">@lang('gettoknowus.mission_desc')</p>
+                                <p class="text-15 text-gray-400 font-medium">@lang('gettoknowus.mission_desc')</p>
                             </div>
                         </div>
                     </div>
@@ -47,8 +47,8 @@
             </div>
 
             <!-- Image Section (right side) -->
-            <div class="col col-6 col-md-6" style="display: flex; align-items: center; justify-content: center;">
-                <img class="w-80percent rounded-20" src="assets/img/Roots-about.png" style="max-width: 100%; height: auto; border-radius: 20px;">
+            <div class="col col-6 col-md-6" style="display: flex;align-items: start;justify-content: start;">
+                <img class="w-70percent rounded-20" src="assets/img/Roots-about.png" style="max-width: 100%; height: auto; border-radius: 20px;">
             </div>
         </div>
     </div>
